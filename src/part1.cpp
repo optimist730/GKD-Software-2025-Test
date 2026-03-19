cceab305b0695dad247fbd99eb74371a70639689
optimist730 <1270683047@qq.com>
2026-03-19 23:29:39 +0800

    finish part1

---
+++ src/part1.cpp
@@ -0,0 +1,71 @@
+#include "part1.hpp"
+
+matrix::matrix(int rows,int cols){
+    this->rows = rows;
+    this->cols = cols;
+    data = std::vector<std::vector<float>> (rows, std::vector<float>(cols, 0));
+}
+
+void matrix::add(matrix other){
+    for(int i = 0;i<rows;i++){
+        for(int j = 0;j<cols;j++){
+            data[i][j] += other.data[i][j];
+        }
+    }
+}
+
+matrix matrix::mul(matrix other){
+    matrix result(rows, other.cols);
+    for(int i=0;i<rows;i++){
+        for(int j=0;j<other.cols;j++){
+            for(int k=0;k<cols;k++){
+                result.data[i][j] += data[i][k] * other.data[k][j];
+            }
+        }
+    }
+    return result;
+}
+
+matrix RELU(matrix a){
+    matrix result(a.rows, a.cols);
+    for(int i=0;i<a.rows;i++){
+        for(int j=0;j<a.cols;j++){
+            result.data[i][j] = max(0.0f, a.data[i][j]);
+        }
+    }
+    return result;
+}
+
+matrix SoftMax(matrix a){
+    matrix result(a.rows,a.cols);
+    float sum=0;
+    for(int i=0;i<a.cols;i++){
+        sum+=exp(a.data[0][i]);
+    }
+    for(int i=0;i<a.cols;i++){
+        result.data[0][i]=exp(a.data[0][i])/sum;
+    }
+    return result;
+}
+
+model::model(int w1r,int w1c,int b1r,int b1c,int w2r,int w2c,int b2r,int b2c){
+    this->w1.rows=w1r;
+    this->w1.cols=w1c;
+    this->b1.rows=b1r;
+    this->b1.cols=b1c;
+    this->w2.rows=w2r;
+    this->w2.cols=w2c;
+    this->b2.rows=b2r;
+    this->b2.cols=b2c;
+}
+
+matrix model::forward(matrix x){
+    matrix first=x.mul(w1);
+    first.add(b1);
+    matrix second=RELU(first);
+    matrix third=second.mul(w2);
+    third.add(b2);
+    matrix result=SoftMax(third);
+    return third;
+
+}
---
+++ src/part1.hpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+class matrix {
+public:
+    matrix();
+    matrix(int rows, int cols);
+
+    void add(matrix other);
+
+    matrix mul(matrix other);
+    int rows, cols;
+    vector<vector<float>> data;
+};
+
+matrix RELU(matrix a);
+
+matrix SoftMax(matrix a);//规定为行向量
+
+class model{
+public:
+    model(int w1r,int w1c,int b1r,int b1c,int w2r,int w2c,int b2r,int b2c);
+
+    matrix forward(matrix x);
+
+    matrix w1,b1,w2,b2;
+};
